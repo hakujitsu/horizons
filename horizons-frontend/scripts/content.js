59f7b6d1cd447694090e709d5401bb2ddc1613da
Kwek Min Yih <minyihkwek@gmail.com>
2023-04-04 15:14:46 +0800

    Add API call to backend to retrieve recommendations

--- horizons-frontend/scripts/content.js
+++ horizons-frontend/scripts/content.js
@@ -1,7 +1,36 @@
 // TODO: add links to the matches [] in manifest.json
-URL = document.location.href;
-// TODO: make API call to backend and store results in localStorage
-// TODO: figure out how to remove stuff from
+url = document.location.href;
+
+async function getRecommendations() {
+  url = document.location.href;
+
+  // TODO: remove entry from localStorage when the page is unloaded
+  addEventListener("beforeunload", (event) => {
+    chrome.storage.local.remove(url)
+  });
+
+  let json;
+  await chrome.storage.local.get("user_id").then((user_id) => {
+    json = JSON.stringify({ user_id, url });
+  });
+
+  const response = await fetch("http://localhost:5000/scrape", {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: json,
+  });
+
+  // TODO: clean up response over here and check that it is stored in storage.local correctly
+  response.json().then(data => {
+    res = JSON.parse(JSON.stringify(data))
+    chrome.storage.local.set({ url: res })
+  });
+}
+
+getRecommendations()
 
 const results = [
   {
